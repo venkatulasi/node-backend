4ef3d03c4161a4db320e9ae3e1c352c1955d5847
venkatulasi <venkatulasi@hotmail.com>
2025-06-21 13:16:42 +0530

    add user and video model

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 
 dotenv.config({
-    path: './env'
+    path: './env'    
 });
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on the port: ${process.env.PORT}`);        
+    })
+})
+.chatch((error) => {
+    console.log("MONGODB connection failed !!", error);    
+})
---
+++ src/models/user.model.js
@@ -0,0 +1,92 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userScehma = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        fullname: {
+            type: String,
+            required: true,        
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, //cloudinary url
+            required: true
+        },
+        coverImage: {
+            type: String,
+            required: true
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Vedio"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is required"]
+        },
+        refreshToken: {
+            type: String
+        },
+
+    },
+    {
+        timestamps: true
+    }
+);
+userScehma.pre("save", async function(next){
+    if(!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10);
+    next()
+})
+
+userScehma.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userScehma.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullname: this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userScehma.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+export const User = mongoose.model("User", userScehma);
